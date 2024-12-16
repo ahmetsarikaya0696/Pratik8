5335a4571a97e20d331a8a6e13128630055f3057
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-12-17 01:07:14 +0300

    Add project files.

---
+++ Pratik8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Kullanıcının girdiği bir limit değerine kadar 0'dan bir sayaç çalıştırıp artırarak, ekrana " Ben bir Patika'lıyım " yazdırınız. 
+// (Sayacınız 0'dan başlayarak 1'er 1'er artacak, koşulunuz ( Sayac <= Limit ) )
+
+Console.Write("Limit : ");
+int.TryParse(Console.ReadLine(), out int limit);
+
+int i = 0;
+while (i <= limit)
+{
+    Console.WriteLine($"{i}) Ben bir Patika'lıyım");
+    i++;
+}
+
+i = 0;
+do
+{
+    Console.WriteLine($"{i}) Ben bir Patika'lıyım");
+    i++;
+} while (i <= limit);
+
+// while döngüsü şartın sağlandığı zaman çalışırken do-while döngüsü şart sağlanmasa dahi bir kez çalışır.
